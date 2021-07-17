03cb2d81a45da3ccc287f9c7c1a23e2ab4f76dbc
Saksham Jaiswal <sjais973@gmail.com>
2021-07-17 18:12:20 +0530

    build complete

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -7,7 +7,7 @@ function Topbar() {
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">Admin</span>
+          <span className="logo">Saksham Jaiswal</span>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
